410760342920aad0f82eabd6decdf41b12fd8b78
mapotso <mapotso04@gmail.com>
2024-11-07 20:12:31 +0200

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Malesela's delicious meal</h1>
+           
+           <img src="foot1.jpeg"  alt="ENJOYY" style={{ width: '100%', height: 'auto' }} />
+           <h1>come and dine with us</h1>
+            
+        </div>
+    );
+};
+
+export default Home;
